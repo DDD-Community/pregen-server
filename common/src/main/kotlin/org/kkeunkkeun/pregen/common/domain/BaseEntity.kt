85b2ba5b574d759d1b0a02dbaa9fc09cb2a7ac03
SoohoLee <hyeri0603@naver.com>
2024-01-25 19:04:16 +0900

    feat: 공통 필드 추상 클래스 추가 (#3)

---
+++ common/src/main/kotlin/org/kkeunkkeun/pregen/common/domain/BaseEntity.kt
@@ -0,0 +1,21 @@
+package org.kkeunkkeun.pregen.common.domain
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@EntityListeners(AuditingEntityListener::class)
+@MappedSuperclass
+abstract class BaseEntity {
+
+    @CreatedDate
+    var createdAt: LocalDateTime? = null
+        protected set
+
+    @LastModifiedDate
+    var modifiedAt: LocalDateTime? = null
+        protected set
+}
