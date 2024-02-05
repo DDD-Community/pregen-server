6ff6b8c9e71451fcc2d8addf8cd340120cda6db2
SoohoLee <hyeri0603@naver.com>
2024-02-05 17:28:12 +0900

    feat: 소셜로그인 탈퇴 api 구현 (#13)

--- account-api/src/main/kotlin/org/kkeunkkeun/pregen/account/service/AccountService.kt
+++ account-api/src/main/kotlin/org/kkeunkkeun/pregen/account/service/AccountService.kt
@@ -9,13 +9,20 @@ import org.kkeunkkeun.pregen.account.domain.dto.AccountUpdateRequest
 import org.kkeunkkeun.pregen.account.domain.dto.NickName
 import org.kkeunkkeun.pregen.account.infrastructure.AccountRepository
 import org.kkeunkkeun.pregen.account.infrastructure.config.AccountProperties
+import org.kkeunkkeun.pregen.account.infrastructure.config.SocialClientProperties
 import org.kkeunkkeun.pregen.account.infrastructure.security.jwt.JwtTokenUtil
 import org.kkeunkkeun.pregen.account.infrastructure.security.jwt.dto.JwtTokenResponse
 import org.kkeunkkeun.pregen.account.infrastructure.security.jwt.refreshtoken.RefreshTokenRepository
 import org.kkeunkkeun.pregen.common.service.JsonConvertor
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.RestTemplate
 import java.io.File
 
 @Transactional(readOnly = true)
@@ -23,18 +30,23 @@ import java.io.File
 class AccountService(
     private val accountRepository: AccountRepository,
     private val refreshTokenRepository: RefreshTokenRepository,
+
     private val jwtTokenUtil: JwtTokenUtil,
+    private val restTemplate: RestTemplate,
     private val jsonConvertor: JsonConvertor,
+
     private val accountProperties: AccountProperties,
+    private val socialProperties: SocialClientProperties,
 ) {
 
     @Transactional
-    fun signUp(email: String, nickName: String, provider: String, role: String): Account {
+    fun signUp(email: String, nickName: String, provider: String, role: String, accessToken: String): Account {
         val account = Account(
             email = email,
             nickName = nickName,
             socialProvider = SocialProvider.isType(provider),
-            role = AccountRole.isType(role)
+            role = AccountRole.isType(role),
+            socialAccessToken = accessToken
         )
         return accountRepository.save(account)
     }
@@ -44,14 +56,14 @@ class AccountService(
         if(SecurityContextHolder.getContext().authentication.principal.equals("anonymousUser")) {
             throw IllegalArgumentException("로그인 상태가 아닙니다.")
         }
-        val accessToken = jwtTokenUtil.extractToken(request.getHeader("accessToken"))
+        val accessToken = jwtTokenUtil.extractToken(request.getHeader("Authorization"))
             ?: throw IllegalArgumentException("accessToken이 존재하지 않습니다.")
         refreshTokenRepository.deleteById(accessToken)
     }
 
     @Transactional
     fun reIssueToken(request: HttpServletRequest): JwtTokenResponse {
-        val refreshToken = jwtTokenUtil.extractToken(request.getHeader("refreshToken"))
+        val refreshToken = jwtTokenUtil.extractToken(request.getHeader("Authorization"))
             ?: throw IllegalArgumentException("refreshToken이 존재하지 않습니다.")
         jwtTokenUtil.verifyToken(refreshToken)
         val authentication = jwtTokenUtil.getAuthentication(refreshToken)
@@ -59,6 +71,18 @@ class AccountService(
 
         return jwtTokenUtil.generateToken(authentication.name, authorities)
     }
+
+    @Transactional
+    fun revokeAccount(request: HttpServletRequest) {
+        val accessToken = jwtTokenUtil.extractToken(request.getHeader("Authorization"))
+            ?: throw IllegalArgumentException("accessToken 존재하지 않습니다.")
+        jwtTokenUtil.verifyToken(accessToken)
+
+        val email = SecurityContextHolder.getContext().authentication.name
+        val account = accountRepository.findByEmail(email) ?: throw IllegalArgumentException("존재하지 않는 계정입니다.")
+        sendRevokeRequest(account.socialAccessToken, account.socialProvider)
+    }
+
 
     fun getMyAccount(email: String): AccountResponse {
         val account = accountRepository.findByEmail(email) ?: throw IllegalArgumentException("존재하지 않는 계정입니다.")
@@ -78,6 +102,30 @@ class AccountService(
             nickName = account.nickName,
             socialProvider = account.socialProvider.value,
         )
+    }
+
+    private fun sendRevokeRequest(accessToken: String, provider: SocialProvider) {
+        val httpHeaders = HttpHeaders()
+        httpHeaders.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        val revokeUrl = when (provider) {
+            SocialProvider.KAKAO -> {
+                httpHeaders.setBearerAuth(accessToken)
+                socialProperties.kakaoRevokeUrl()
+            }
+            SocialProvider.NAVER -> {
+                socialProperties.naverRevokeUrl(accessToken, SocialProvider.NAVER.value)
+            }
+            SocialProvider.GOOGLE -> {
+                socialProperties.googleRevokeUrl(accessToken)
+            }
+        }
+        val httpEntity: HttpEntity<String> = HttpEntity(revokeUrl, httpHeaders)
+        val response = restTemplate.exchange(revokeUrl, HttpMethod.POST, httpEntity, String::class.java)
+
+        if (!response.statusCode.is2xxSuccessful) {
+            throw IllegalArgumentException("회원 탈퇴에 실패하였습니다. 다시 시도해주십시오.")
+        }
     }
 
     fun findByEmail(email: String): Account? {
