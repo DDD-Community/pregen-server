be6d8e0fc3600b76960dc365c43a36aa63d34112
SoohoLee <hyeri0603@naver.com>
2024-02-21 15:20:23 +0900

    refactoring: sendTo variable 적용 불가, 경로 직접 지정 (#24)

--- practice-websocket/src/main/kotlin/org/kkeunkkeun/pregen/practice/presentation/PracticeWebSocketController.kt
+++ practice-websocket/src/main/kotlin/org/kkeunkkeun/pregen/practice/presentation/PracticeWebSocketController.kt
@@ -1,32 +1,39 @@
 package org.kkeunkkeun.pregen.practice.presentation
 
 import org.kkeunkkeun.pregen.practice.service.MessageHandler
+import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Controller
 
 @Controller
 class PracticeWebSocketController(
     private val messageHandler: MessageHandler,
+    val messageTemplate: SimpMessagingTemplate,
 ) {
 
     @MessageMapping("/practice/{sessionId}")
-    @SendTo("/sub/practice/{sessionId}")
     fun enter(
+        @DestinationVariable sessionId: String,
         @Payload request: BaseMessage,
         headerAccessor: SimpMessageHeaderAccessor,
     ): BaseMessage {
-        return messageHandler.handle(request)
+        val handle = messageHandler.handle(request)
+        messageTemplate.convertAndSend("/sub/practice/$sessionId", handle)
+        return handle
     }
 
     @MessageMapping("/practice/{sessionId}/ping")
-    @SendTo("/sub/practice/{sessionId}")
     fun healthCheck(
+        @DestinationVariable sessionId: String,
         @Payload request: BaseMessage,
         headerAccessor: SimpMessageHeaderAccessor,
     ): BaseMessage {
-        return BaseMessage(request.sessionId, "HEALTH_CHECK", "Pong!")
+        val baseMessage = BaseMessage(request.sessionId, "HEALTH_CHECK", "Pong!")
+        messageTemplate.convertAndSend("/sub/practice/$sessionId", baseMessage)
+        return baseMessage
     }
 }
